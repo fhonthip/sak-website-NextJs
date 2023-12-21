1409bad19a42c1a9ecbaaa8f38607d445f67fe7f
FHONTHIP.Y <fhonthip@sakww.com>
2023-12-21 14:48:49 +0700

    new files and folders
    new files and folders

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"37GPGGIABF1xe8xO83cuLjEHbR+EejVpsNxMzQRwKdo=\"\n}"
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>Home
+      <p>
+         NextJs test Github 2.0
+      </p>
+    </div>
+  )
+}
+
+export default page
